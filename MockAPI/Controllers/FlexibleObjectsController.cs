0b9f1897a1863a28c80a28b5027b47aaf035ef19
asipahi <48097044+asipahi@users.noreply.github.com>
2022-01-19 14:05:28 -0500

    default names

--- MockAPI/Controllers/FlexibleObjectsController.cs
+++ MockAPI/Controllers/FlexibleObjectsController.cs
@@ -15,20 +15,35 @@ namespace MockAPI.Controllers
     {
         public List<string> types = new List<string> { "ManualBeginningApproval", "ApprovalChainApproval" };
         public List<string> statuses = new List<string> { "pending_approval", "approved", "escalated", "submitted" };
+        public List<string> names = new List<string> {
+            "Kylie Bailey",
+            "Maria Howard",
+            "Christopher Vance",
+            "Irene Peake",
+            "Evan Thomson",
+            "Michael Gibson",
+            "Olivia Skinner",
+            "Gavin Hodges",
+            "Blake Sanderson",
+            "Piers Watson" 
+        };
 
         [HttpGet("instances")]
         [HttpPost("{transactionType}/instances")]
         [HttpPost("instances")]
         [HttpPut("{transactionType}/instances/{id:int}")]
+        [HttpPut("{transactionType}/instances/{id:int}/approve")]
         public FlexibleObjectApproversDto Instance(string transactionType)
         {
             var obj = RandomValue.Object<FlexibleObjectApproversDto>();
-            var position = 1;
+            var position = 0;
             foreach (var item in obj.approvals)
             {
                 item.type = types[RandomValue.Int(1, 0)];
-                item.status = statuses[RandomValue.Int(3, 0)];
-                position += 1;
+                item.status = statuses[RandomValue.Int(statuses.Count - 1, 0)];
+                item.position = position++;
+                item.approver.full_name = names[RandomValue.Int(names.Count - 1, 0)];
+                item.
             }
             return obj;
         }
